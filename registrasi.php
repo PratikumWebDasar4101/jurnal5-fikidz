0f4d93f868dc6b97f08391128a3c722840958f0d
Fiki Dzulfiqar <33850678+fikidz@users.noreply.github.com>
2018-10-01 10:02:05 +0700

    Update registrasi.php

--- registrasi.php
+++ registrasi.php
@@ -1,11 +1,31 @@
-<!DOCTYPE html>
-<html>
-<head>
-	<title>Form Registrasi</title>
-</head>
-<body>
-
-
-
-</body>
-</html>
+<form action="" method="POST">
+	<table align="center">
+		<tr>
+			<td colspan="3" align="center" bgcolor="lightblue"><h2>Form Registrasi</h2></td>
+		</tr>
+		<tr>
+			<td>NIM</td>
+			<td>:</td>
+			<td><input type="text" name="nim" maxlength="10" pattern="\d*"></td>
+		</tr>
+		<tr>
+			<td><br></td>
+		</tr>
+		<tr>
+			<td>Nama</td>
+			<td>:</td>
+			<td><input type="text" name="nama" maxlength="25"></td>
+		</tr>
+		<tr>
+			<td><br></td>
+		</tr>
+		<tr>
+			<td>Email</td>
+			<td>:</td>
+			<td><input type="Email" name="email" placeholder="example@gmail.com"></td>
+		</tr>
+		<tr>
+			<td colspan="3"><input type="submit" name="submit" value="Submit"></td>
+		</tr>
+	</table>
+</form>
