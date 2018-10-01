7e24bc14de323f64c34268a22e029843c2563ecb
Fiki Dzulfiqar <33850678+fikidz@users.noreply.github.com>
2018-10-01 10:41:17 +0700

    Update prosesregis.php

--- prosesregis.php
+++ prosesregis.php
@@ -1,7 +1,14 @@
-<?php
+<?php 
+session_start();
 
+if (isset($_POST['nama'])) {
+	$nim = $_POST['nim'];
+	$nama = $_POST['nama'];
+	$email = $_POST['email'];
 
+	echo $nim;
+	echo $nama;
+	echo $email;
+}
 
-
-
-?>
+ ?>
