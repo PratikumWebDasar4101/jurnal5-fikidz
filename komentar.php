d5a0617e507815db8b4b0e376dbb7fd01906e416
Fiki Dzulfiqar <33850678+fikidz@users.noreply.github.com>
2018-10-01 10:03:24 +0700

    Update komentar.php

--- komentar.php
+++ komentar.php
@@ -1,7 +1,32 @@
-<?php
-//untuk yg komentar kalian bebas mau menggunakan 2 file atau satu file php
+<form method="POST">
+	<table align="center">
+		<tr>
+			<td colspan="2" align="center"><h2>Silahkan Berkomentar</h2></td>
+		</tr>
+		<tr>
+			<td>Nama</td>
+			<td><input type="text" name="nama" maxlength="25" required></td>
+		</tr>
+		<tr>
+			<td colspan="2"><textarea placeholder="Masukkan Komentar min 5 Karakter" cols="50" rows="5" name="komentar"></textarea></td>
+		</tr>
+		<tr>
+			<td colspan="2"><br><input type="submit" name="kirim" value="Kirim"></td>
+		</tr>
+	</table>
+</form>
 
+<?php 
+if (isset($_POST['nama'])) {
+	$nama = $_POST['nama'];
+	$komentar = $_POST['komentar'];
+	$count = strlen($komentar);
 
+	if ($count < 5) {
+		echo "Jumlah Komentar Sedikit";
+	} else{
+		echo "Jumlah Komentar Anda Sesuai : ".$count." Karakter";
+	}
+}
 
-
-?>
+ ?>
